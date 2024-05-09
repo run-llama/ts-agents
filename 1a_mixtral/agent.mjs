cd4c7bcac9f29b6cfd46672c5eba88517fbead9b
Laurie Voss <github@seldo.com>
2024-05-08 17:05:55 -0700

    Mixtral ReAct agent

---
+++ 1a_mixtral/agent.mjs
@@ -0,0 +1,65 @@
+import { 
+    Ollama, 
+    FunctionTool, 
+    ReActAgent,
+    Settings
+} from "llamaindex"
+import 'dotenv/config'
+
+Settings.llm = new Ollama({ 
+    model: "mixtral:8x7b", 
+    temperature: 0.1
+});
+
+/*
+  Set up logging so we can see the work in progress.
+  Available events:
+  llm-start
+  llm-end
+  agent-start
+  agent-end
+  llm-tool-call
+  llm-tool-result
+*/
+Settings.callbackManager.on("llm-tool-call", (event) => {
+    console.log(event.detail.payload)
+})
+Settings.callbackManager.on("llm-tool-result", (event) => {
+    console.log(event.detail.payload)
+})
+
+const sumNumbers = ({a, b}) => {
+    return `${a + b}`;
+}
+
+const tools = [
+    FunctionTool.from(
+        sumNumbers,
+        {
+            name: "sumNumbers",
+            description: "Use this function to sum two numbers",
+            parameters: {
+                type: "object",
+                properties: {
+                    a: {
+                        type: "number",
+                        description: "First number to sum"
+                    },
+                    b: {
+                        type: "number",
+                        description: "Second number to sum"
+                    },
+                },
+                required: ["a", "b"]
+            }
+        }
+    )
+]
+
+const agent = new ReActAgent({tools})
+
+let response = await agent.chat({
+    message: "Add 101 and 303",
+})
+
+console.log(response)
